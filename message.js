192bf9c0eb637e91f5a1028f5ba02d4da337e628
Alexander Marini <svenskost6@gmail.com>
2024-02-09 17:37:40 +0100

    CORS problem finally worked around

---
+++ message.js
@@ -0,0 +1,6 @@
+const message = {
+   // Ersätt null med din text.
+   // Glöm ej situationstecken runt din nya text.
+   title: null,
+   text: null,
+}
--- scripts/message.js
+++ scripts/message.js
@@ -1,34 +1,33 @@
-const message = document.getElementById('message')
+const messageContainer = document.getElementById('messageContainer')
 const titleEl = document.getElementById('title')
 const textEl = document.getElementById('text')
 
-const titlePlaceholder = 'Ditt meddelande'
-const textPlaceholder =
-   'Här visas ditt personliga meddelande till mottagaren av webbsidan. Följ instruktionerna i guide.txt för att skriva ditt meddelande.'
-
-const titleContent = titleEl.innerHTML
-const textContent = textEl.innerHTML
+const titlePlaceholder = titleEl.innerHTML
+const textPlaceholder = textEl.innerHTML
 
 titleEl.innerHTML = ''
 textEl.innerHTML = ''
 
 setTimeout(() => {
-   message.style.scale = 1
-   message.style.opacity = 1
+   messageContainer.style.scale = 1
+   messageContainer.style.opacity = 1
    setTimeout(() => {
       showMessage()
    }, 1000)
 }, 500)
 
 const showMessage = () => {
-   if (titleContent != 'Skriv din titel här') {
-      write(titleEl, titleContent)
+   let title = message.title
+   let text = message.text
+
+   if (title != null) {
+      write(titleEl, title)
    } else {
       write(titleEl, titlePlaceholder)
    }
 
-   if (textContent != 'Skriv din text här') {
-      write(textEl, textContent)
+   if (text != null) {
+      write(textEl, text)
    } else {
       write(textEl, textPlaceholder)
    }
@@ -47,8 +46,8 @@ const linkTransition = link => {
    if (link == 'index') {
       navbar.style.opacity = 0
    }
-   message.style.transition = '0.5s'
-   message.style.opacity = 0
+   messageContainer.style.transition = '0.5s'
+   messageContainer.style.opacity = 0
    setTimeout(() => {
       if (link == 'index') {
          document.location.href = '../index.html'
